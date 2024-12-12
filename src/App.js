187e8948488c369dd618eba7d06abef07ef1dcea
michaelferry25 <g00420185@atu.ie>
2024-12-12 15:40:57 +0000

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import {browserRouter as Router,Route, Routes} from "react-router-dom"
 import Homepage from './pages/HomePage/Homepage';
+import Recipes from './pages/Recipes/Recipes';
+import RecipeDetails from './pages/Recipe Details/RecipeDetails';
+import FavouriteRecipe from './pages/Favourite Recipes/FavouriteRecipes';
 
 function App() {
   return (
     <Routes>
       <Route path= "/" element={<Homepage />}></Route>
+      <Route path= "/favourites" element={<FavouriteRecipe />}></Route>
+      <Route path= "/recipes" element={<Recipes />}></Route>
+      <Route path= "/recipe/:id" element={<RecipeDetails />}></Route>
     </Routes>
   );
 }
