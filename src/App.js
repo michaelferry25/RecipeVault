7fe203ec387d48d4f39c13d3ee62088f79135531
michaelferry25 <g00420185@atu.ie>
2024-12-12 23:37:12 +0000

    Navbar has a dropdown box now and replaces the img with an .svg and cleaned up the homepage.css file and added a few imports to index.js and app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Recipes from './pages/Recipes/Recipes';
 import RecipeDetails from './pages/Recipe Details/RecipeDetails';
 import FavouriteRecipe from './pages/Favourite Recipes/FavouriteRecipes';
 import CreateRecipe from "./pages/CreateRecipe/CreateRecipe";
+import SearchPage from "./pages/search/SearchPage";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       <Route path= "/" element={<Homepage />}></Route>
       <Route path= "/favourites" element={<FavouriteRecipe />}></Route>
       <Route path= "/recipes" element={<Recipes />}></Route>
-      <Route path= "/recipe/:id" element={<RecipeDetails />}></Route>
+        <Route path= "/recipe/:id" element={<RecipeDetails />}></Route>
+        <Route path= "/search/:query" element={<SearchPage />}></Route>
       <Route path="/create-recipe" element={<CreateRecipe />} />
     </Routes>
   );
--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from "./components/Header/Header";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+        <Header/>
     <App />
     </BrowserRouter>
   </React.StrictMode>
